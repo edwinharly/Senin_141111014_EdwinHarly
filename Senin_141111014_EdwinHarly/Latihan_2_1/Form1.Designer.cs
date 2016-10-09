aaf557a1076c466f18ffafbbb796d91b3b55eeea
Edwin <edwinharly@live.com>
2016-10-09 22:37:50 +0700

    Latihan_2_1 sudah selesai

--- Senin_141111014_EdwinHarly/Latihan_2_1/Form1.Designer.cs
+++ Senin_141111014_EdwinHarly/Latihan_2_1/Form1.Designer.cs
@@ -105,6 +105,7 @@
             this.button2.TabIndex = 5;
             this.button2.Text = "&Hapus";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
--- Senin_141111014_EdwinHarly/Latihan_2_1/Form1.cs
+++ Senin_141111014_EdwinHarly/Latihan_2_1/Form1.cs
@@ -20,35 +20,68 @@ namespace Latihan_2_1
         private void Form1_Load(object sender, EventArgs e)
         {
             /// Bold setiap hari Sabtu & Minggu
-            DateTime tmp = new DateTime(2016,1,2);
-            while(tmp.Year<=2016)
+            DateTime tmp1 = new DateTime(2016, 1, 2);
+            DateTime tmp2 = new DateTime(2016, 1, 3);
+            for (int i=0; i<53; i++)
             {
-                if (tmp.DayOfWeek == DayOfWeek.Saturday || tmp.DayOfWeek == DayOfWeek.Sunday)
-                {
-                    monthCalendar1.AddBoldedDate(tmp);
-                    tmp.AddDays(1);
-                }
-                else
-                {
-                    tmp.AddDays(5);
-                }
+                monthCalendar1.AddBoldedDate(tmp1);
+                monthCalendar1.AddBoldedDate(tmp2);
+                tmp1 = tmp1.AddDays(7);
+                tmp2 = tmp2.AddDays(7);
             }
-
-
+            
+            // Hari libur nasional 2016
+            DateTime[] hariLibur = new DateTime[15];
+            hariLibur[0] = new DateTime(2016, 1, 1);
+            hariLibur[1] = new DateTime(2016, 2, 8);
+            hariLibur[2] = new DateTime(2016, 3, 9);
+            hariLibur[3] = new DateTime(2016, 3, 25);
+            hariLibur[4] = new DateTime(2016, 5, 1);
+            hariLibur[5] = new DateTime(2016, 5, 5);
+            hariLibur[6] = new DateTime(2016, 5, 6);
+            hariLibur[7] = new DateTime(2016, 5, 22);
+            hariLibur[8] = new DateTime(2016, 6, 7);
+            hariLibur[9] = new DateTime(2016, 7, 7);
+            hariLibur[10] = new DateTime(2016, 8, 17);
+            hariLibur[11] = new DateTime(2016, 9, 12);
+            hariLibur[12] = new DateTime(2016, 10, 2);
+            hariLibur[13] = new DateTime(2016, 12, 12);
+            hariLibur[14] = new DateTime(2016, 12, 25);
+            foreach(DateTime i in hariLibur)
+            {
+                monthCalendar1.AddBoldedDate(i);
+            }
+            monthCalendar1.UpdateBoldedDates();
             
         }
 
-        /// Penambahan bolded date melalui tombol tambah
+        /// Penambahan bolded date melalui tombol Tambah
         private void button1_Click(object sender, EventArgs e)
         {
             try
             {
                 DateTime tmp = new DateTime(2016, domainUpDown1.SelectedIndex + 1, (int)numericUpDown1.Value);
-                monthCalendar1.AddAnnuallyBoldedDate(tmp);
+                monthCalendar1.AddBoldedDate(tmp);
+                monthCalendar1.UpdateBoldedDates();
             }
-            catch (Exception e)
+            catch (Exception err)
             {
+                MessageBox.Show("Maaf, tanggal yang anda pilih tidak tersedia, silakan coba lagi");
+            }
+        }
 
+        /// Penghapusan bolded date melalui tombol Hapus
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DateTime tmp = new DateTime(2016, domainUpDown1.SelectedIndex + 1, (int)numericUpDown1.Value);
+                monthCalendar1.RemoveBoldedDate(tmp);
+                monthCalendar1.UpdateBoldedDates();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Maaf, tanggal yang anda pilih tidak tersedia, silakan coba lagi");
             }
         }
     }
